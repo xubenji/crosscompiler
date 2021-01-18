8caad3044db831f53603114c70a406bb31252a7d
xubenji <459547070@qq.com>
2021-01-19 05:08:56 +0800

    gcc8.1.0

--- buildtools.sh
+++ buildtools.sh
@@ -3,7 +3,7 @@ apt-get install -y gcc make build-essential bison flex libgmp3-dev libmpc-dev 	l
 clang --target=aarch64-elf
 
 #wget https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz
-#wget https://ftpmirror.gnu.org/gcc/gcc-7.3.0/gcc-7.3.0.tar.gz
+wget https://ftpmirror.gnu.org/gcc/gcc-8.1.0/gcc-8.1.0.tar.gz
 #wget https://ftpmirror.gnu.org/mpfr/mpfr-4.0.1.tar.gz
 #wget https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.bz2
 #wget https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz
