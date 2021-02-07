bddc4f24bafe67a8d66aece227e36b9167b657b9
XU, yanjie <459547070@qq.com>
2021-02-08 02:28:35 +0800

    Update buildqemu.sh

--- buildqemu.sh
+++ buildqemu.sh
@@ -1,6 +1,7 @@
 apt-get install -y gcc make build-essential bison flex libgmp3-dev libmpc-dev 	libmpfr-dev texinfo libisl-dev clang *zlib zlib*
 apt-get install build-essential zlib1g-dev pkg-config libglib2.0-dev binutils-dev libboost-all-dev autoconf libtool libssl-dev libpixman-1-dev libpython-dev python-pip python-capstone virtualenv -y
 apt-get install libsdl1.2-dev -y
+apt-get install libpixman-1-dev
 
 cd ./qemu-2.11.0
 ./configure --target-list=aarch64-softmmu --enable-modules --enable-tcg-interpreter --enable-debug-tcg --python=/usr/bin/python2.7
