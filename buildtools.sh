c68cf656bb22dc27bc47bd4f9205fdfebd71a8c7
Yanjie Xu <459547070@qq.com>
2021-10-26 17:41:22 -0400

    Update buildtools.sh

--- buildtools.sh
+++ buildtools.sh
@@ -1,14 +1,14 @@
-apt-get install -y gcc make build-essential bison flex libgmp3-dev libmpc-dev 	libmpfr-dev texinfo libisl-dev clang *zlib zlib*
+apt-get install -y gcc make build-essential bison flex libgmp3-dev libmpc-dev 	libmpfr-dev texinfo libisl-dev clang *zlib zlib* nasm
 apt-get install -y gdb-multiarch
 #clang --target=aarch64-elf
 
-#wget https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz
+wget https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz
 wget https://ftpmirror.gnu.org/gcc/gcc-8.1.0/gcc-8.1.0.tar.gz
-#wget https://ftpmirror.gnu.org/mpfr/mpfr-4.0.1.tar.gz
-#wget https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.bz2
-#wget https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz
-#wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2
-#wget https://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
+wget https://ftpmirror.gnu.org/mpfr/mpfr-4.0.1.tar.gz
+wget https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.bz2
+wget https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz
+wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2
+wget https://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
 
 for i in *.tar.gz; do tar -xzf $i; done
 for i in *.tar.bz2; do tar -xjf $i; done
